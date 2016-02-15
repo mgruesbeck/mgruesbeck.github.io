36e8a823076f4f454ebbd04782e3874a0e525d46
mgruesbeck <mgruesbeck@gmail.com>
2016-02-14 22:08:18 -0800

    move templates folder

--- s3-mgruesbeck/app.js
+++ s3-mgruesbeck/app.js
@@ -5,19 +5,19 @@ var browserSync = require('metalsmith-browser-sync');
 var handlebars = require('handlebars');
 var fs = require('fs');
 
-handlebars.registerPartial('header', fs.readFileSync(__dirname + '/templates/header.hbt').toString());
-handlebars.registerPartial('footer', fs.readFileSync(__dirname + '/templates/footer.hbt').toString());
+handlebars.registerPartial('header', fs.readFileSync(__dirname + '/src/templates/header.hbt').toString());
+handlebars.registerPartial('footer', fs.readFileSync(__dirname + '/src/templates/footer.hbt').toString());
 
 Metalsmith(__dirname)
     .use(markdown())
     .use(browserSync({
-        server : "build",
-        files  : ["src/**/*.md", "src/**/*.css", "templates/**/*.hbt"]
+        server: 'build',
+        files: ['src/**/*.md', 'src/**/*.css', 'templates/**/*.hbt']
     }))
     .use(layouts({
         engine: 'handlebars',
-        directory: 'templates'}
-        ))
+        directory: 'src/templates'
+    }))
     .destination('./build')
     .build(function(err) {
         if (err) throw err;
